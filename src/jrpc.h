d31105d0a18cfa142e8a1320d7475b65e0c26566
Alex Sherman <asherman1024@gmail.com>
2016-06-13 16:30:03 -0500

    Got things to compile with new C++ style

--- src/jrpc.h
+++ src/jrpc.h
@@ -8,37 +8,34 @@
 #include <json/json.h>
 #include "uthash.h"
 
-struct jrpc_proxy
-{
-    struct sockaddr_storage remote;
-    json_object *result;
-    int sockfd;
-    int next_id;
-};
+namespace MRPC {
 
-struct jrpc_server
-{
-    int listen_sockfd;
-    struct jrpc_server_method *methods;
-    volatile int running;
-    int client_sockfd;
-};
+    class Transport;
+    class Service;
 
-typedef json_object *(*jrpc_function)(json_object *);
+    class Node {
+    public:
+        Node();
+        std::string guid;
+    private:
+        std::vector<MRPC::Transport> transports;
+        std::map<std::string, Service> services;
+    };
+    class Transport {
 
-struct jrpc_server_method
-{
-    char name[128];
-    jrpc_function function;
-    UT_hash_handle hh;
-};
+    };
+    class Proxy {
+    public:
+        Proxy(std::string host);
+        struct sockaddr_storage remote;
+        int sockfd;
+        int next_id;
+    };
+    class Service {
 
-struct jrpc_server *jrpc_server_init(uint16_t port);
-int jrpc_server_register(struct jrpc_server *server, jrpc_function func, char *name);
-void jrpc_server_run(struct jrpc_server *server);
-
-struct jrpc_proxy *jrpc_proxy_init(char *host, uint16_t port);
-int jrpc_proxy_close(struct jrpc_proxy *proxy);
-int jrpc_proxy_call(struct jrpc_proxy *proxy, const char *method_name, const char *parameters, ...);
+    };
+    Node LocalNode;
+    
+}
 
 #endif //_JRPC_H_
